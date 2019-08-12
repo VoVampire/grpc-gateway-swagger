efa06344cf34db17574c7ac43d885d5930b08328
zhangkai <zhangkai1@mintq.com>
2019-08-12 14:27:53 +0800

    add swagger

--- cmd/grpc-gateway/main.go
+++ cmd/grpc-gateway/main.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	assetfs "github.com/elazarl/go-bindata-assetfs"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
 	hello "grpc-gateway-swagger/protos"
+	"grpc-gateway-swagger/swagger"
 	"log"
 	"net/http"
 )
@@ -15,6 +17,22 @@ var (
 	grpcServerGateway  = flag.String("grpc-server-gateway", ":8081", "gRPC server gateway")
 	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:9090", "gRPC server endpoint")
 )
+
+func serveSwaggerJson(mux *http.ServeMux) {
+	fileServer := http.FileServer(http.Dir("/home/qydev/"))
+	prefix := "/swagger.json/"
+	mux.Handle(prefix, http.StripPrefix(prefix, fileServer))
+}
+
+func serveSwaggerUI(mux *http.ServeMux) {
+	fileServer := http.FileServer(&assetfs.AssetFS{
+		Asset:    swagger.Asset,
+		AssetDir: swagger.AssetDir,
+		Prefix:   "/home/qydev/go/src/grpc-gateway-swagger/swagger/swagger-ui",
+	})
+	prefix := "/swagger-ui/"
+	mux.Handle(prefix, http.StripPrefix(prefix, fileServer))
+}
 
 func run() error {
 	ctx := context.Background()
@@ -30,8 +48,13 @@ func run() error {
 		return err
 	}
 
+	httpMux := http.NewServeMux()
+	httpMux.Handle("/", mux)
+	serveSwaggerUI(httpMux)
+	serveSwaggerJson(httpMux)
+
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
-	return http.ListenAndServe(*grpcServerGateway, mux)
+	return http.ListenAndServe(*grpcServerGateway, httpMux)
 }
 
 func main() {
